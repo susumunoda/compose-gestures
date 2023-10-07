4844ab0c65ba1d7e28c0f8dc982f894c3343409b
Susumu Noda <susumunoda@gmail.com>
2023-10-07 14:23:54 -0700

    Remove placeholder test code

--- compose-gestures/src/androidUnitTest/kotlin/com/susumunoda/compose/gestures/androidTest.kt
+++ compose-gestures/src/androidUnitTest/kotlin/com/susumunoda/compose/gestures/androidTest.kt
@@ -1,12 +1,11 @@
 package com.susumunoda.compose.gestures
 
-import org.junit.Assert.assertTrue
 import org.junit.Test
 
 class AndroidGreetingTest {
 
     @Test
     fun testExample() {
-        assertTrue("Check Android is mentioned", Greeting().greet().contains("Android"))
+        // TODO
     }
 }
--- compose-gestures/src/iosTest/kotlin/com/susumunoda/compose/gestures/iosTest.kt
+++ compose-gestures/src/iosTest/kotlin/com/susumunoda/compose/gestures/iosTest.kt
@@ -1,12 +1,11 @@
 package com.susumunoda.compose.gestures
 
 import kotlin.test.Test
-import kotlin.test.assertTrue
 
 class IosGreetingTest {
 
     @Test
     fun testExample() {
-        assertTrue(Greeting().greet().contains("iOS"), "Check iOS is mentioned")
+        // TODO
     }
 }
